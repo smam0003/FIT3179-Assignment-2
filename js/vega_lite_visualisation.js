e829fd648db29973ff69dd69938db87b5d884310
smam0003 <smam0003@student.monash.edu>
2021-10-06 08:07:50 +0700

    Pretty formatting

--- js/vega_lite_visualisation.js
+++ js/vega_lite_visualisation.js
@@ -1,3 +1,4 @@
 var vg_1 = "./js/choropleth_map_attendance_tally.vg.json";
-    vegaEmbed("#choropleth_map", vg_1).then(function(result) {
-    }).catch(console.error);
+vegaEmbed("#choropleth_map", vg_1)
+  .then(function (result) {})
+  .catch(console.error);
