2fb2d41bdc85113534112d6fa8ebc909299c91e0
smam0003 <smam0003@student.monash.edu>
2021-10-09 14:50:28 +0700

    Integrated second bump chart

--- js/vega_lite_visualisation.js
+++ js/vega_lite_visualisation.js
@@ -2,3 +2,8 @@ var vg_1 = "./js/choropleth_map_attendance_tally.vg.json";
 vegaEmbed("#choropleth_map", vg_1)
   .then(function (result) {})
   .catch(console.error);
+
+var vg_2 = "./js/bump_chart_medal_rankings.vg.json";
+vegaEmbed("#bump_chart", vg_2)
+  .then(function (result) {})
+  .catch(console.error);
